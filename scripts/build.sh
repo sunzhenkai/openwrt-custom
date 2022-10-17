64584b634630dd3bfc7aa5ed8b6b00f477e667c3
sunzhenkai <zhenkai.sun@qq.com>
2022-10-17 12:10:51 +0800

    fix

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,15 +1,17 @@
 #!/bin/bash
 git submodule update --init --recursive
 
-# config theme
+# theme
 cp -r theme/luci-theme-argon lede/package/lean/
+
+# config
+cp config/lede-config lede/.config
 
 # build lede
 cd lede || exit 1
 ./scripts/feeds update -a
 ./scripts/feeds install -a
-# config
-cp config/lede-config lede/.config
+
 # compile
 make download -j$(nproc)
 make V=s -j$(nproc)
