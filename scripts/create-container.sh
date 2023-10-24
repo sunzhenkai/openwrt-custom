51694f58b702d7ac88b5778b8604bb2f84578fb5
zhenkai.sun <zhenkai.sun@qq.com>
2023-10-24 22:53:59 +0800

    fix create container

--- scripts/create-container.sh
+++ scripts/create-container.sh
@@ -2,4 +2,4 @@
 BASE=$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &>/dev/null && pwd)
 cd "$BASE/.." || exit 1
 
-docker run -it -p 2022:22 -d -v "$(PWD)":/data --privileged --name=lede-builder zhenkai.sun/lede-builder
+docker run -it -p 2022:22 -d -v "$(pwd)":/data --privileged --name=lede-builder zhenkai.sun/lede-builder
